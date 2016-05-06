868d651e1820e061e4384287c76f7cfebc10aa67
stephen-standridge <stephen@formisfunction.io>
2016-05-06 12:59:09 -0700

    Add markdown rendering on text pieces
    Change linear navigation styling.

--- assets/documentation/welcome.js
+++ assets/documentation/welcome.js
@@ -5,7 +5,24 @@ module.exports = {
 	],
 	center: [
 		{ type: 'TextPiece',
-			body: 'testing this document body' },
+			body: `Lorem ipsum dolor sit amet, vel ex doming prodesset. Sea cu solet quodsi, labitur prodesset no mel, ea homero legere recusabo his. Id mucius atomorum eum. Ea nec justo primis facete, ne duo salutandi voluptatum consectetuer, vim at nihil choro. Est et democritum expetendis interesset, ea wisi timeam instructior nam. Debet zril facilisis te has, ei quem dissentiet mea.
+
+Feugiat praesent adversarium ius te, feugiat tacimates quaestio cum at. Ut per omnis appareat, vidit libris albucius et quo. Et duo dicit labores, mea eu assum putant, mea ut iriure pertinax. Harum indoctum iracundia est ne. Tibique hendrerit repudiandae has id.
+
+Est at choro alterum, at alia pericula vix. Aeterno sententiae ut has, sed tempor luptatum electram ad, vel an essent vivendo. Sale elitr lucilius has ad. Ne his expetenda disputando. Mei at sumo viris fabellas.
+
+Esse nulla vulputate sit ad. Sed error malorum eu, te per soluta deleniti assentior. Veri saperet civibus ex per. Nam eu agam nostrum.
+
+Nulla libris temporibus ea has, ex qui discere deterruisset. Per denique menandri repudiare et, duo id impedit fierent consetetur, ei qui fuisset eloquentiam. Urbanitas liberavisse at cum, populo nemore ne sea. Eum an summo iriure suscipiantur. Duo ut tota eius, est ut sint sapientem. Regione vituperatoribus ei eam. Cu usu scripserit accommodare, ius nusquam fuisset imperdiet eu, quo regione malorum persequeris te. 
+Lorem ipsum dolor sit amet, vel ex doming prodesset. Sea cu solet quodsi, labitur prodesset no mel, ea homero legere recusabo his. Id mucius atomorum eum. Ea nec justo primis facete, ne duo salutandi voluptatum consectetuer, vim at nihil choro. Est et democritum expetendis interesset, ea wisi timeam instructior nam. Debet zril facilisis te has, ei quem dissentiet mea.
+
+Feugiat praesent adversarium ius te, feugiat tacimates quaestio cum at. Ut per omnis appareat, vidit libris albucius et quo. Et duo dicit labores, mea eu assum putant, mea ut iriure pertinax. Harum indoctum iracundia est ne. Tibique hendrerit repudiandae has id.
+
+Est at choro alterum, at alia pericula vix. Aeterno sententiae ut has, sed tempor luptatum electram ad, vel an essent vivendo. Sale elitr lucilius has ad. Ne his expetenda disputando. Mei at sumo viris fabellas.
+
+Esse nulla vulputate sit ad. Sed error malorum eu, te per soluta deleniti assentior. Veri saperet civibus ex per. Nam eu agam nostrum.
+
+Nulla libris temporibus ea has, ex qui discere deterruisset. Per denique menandri repudiare et, duo id impedit fierent consetetur, ei qui fuisset eloquentiam. Urbanitas liberavisse at cum, populo nemore ne sea. Eum an summo iriure suscipiantur. Duo ut tota eius, est ut sint sapientem. Regione vituperatoribus ei eam. Cu usu scripserit accommodare, ius nusquam fuisset imperdiet eu, quo regione malorum persequeris te.` },
 		{ type: 'LinearNavigation' }
 	],
 }
--- source/components/pieces/TextPiece.jsx
+++ source/components/pieces/TextPiece.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
+import Remarkable from 'remarkable'
+const MD = new Remarkable();
 
 class TextPiece extends React.Component {
 	render(){
-		return (	<div className='textPiece piece'>
-				<h1> text </h1>
-				<p>{this.props.body}</p>
+		return (	<div className='textPiece piece' dangerouslySetInnerHTML={ {__html: this.renderMarkdown() } }>
 			</div> )		
+	}
+	renderMarkdown(){
+		return MD.render(this.props.body)
 	}
 }
 
