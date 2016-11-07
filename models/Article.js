040dbb6d2c874a4aa04b0f76682ee779d9968ce8
stephen-standridge <stephen@formisfunction.io>
2016-11-07 09:12:29 -0800

    add postinstall, remove cloudinary from article

--- models/Article.js
+++ models/Article.js
@@ -15,7 +15,6 @@ Article.add({
 	title: { type: String, required: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-	image: { type: Types.CloudinaryImage },
 	content: { type: Types.Html, wysiwyg: true, height: 400 }
 });
 
