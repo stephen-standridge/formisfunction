17849333e3babc8ff4594bd2f82aa2d8ab8b7a73
stephen-standridge <stephen@formisfunction.io>
2017-01-21 12:04:21 -0800

    reformat webpack prod files

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -2,6 +2,9 @@ var webpack = require('webpack')
 var S3Plugin = require('webpack-s3-plugin')
 var config = require('./webpack.core.js');
 
+var s3BucketName = 'component_api_client.com'
+var apiHost = 'localhost:3000/api/v1'
+
 var s3Plugin = new S3Plugin({
   // Only upload css and js 
   include: /.*\.(css|js|html)/,
@@ -11,12 +14,12 @@ var s3Plugin = new S3Plugin({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
   s3UploadOptions: {
-    Bucket: process.env.AWS_BUCKET
+    Bucket: s3BucketName
   }
 });
 
 var apiHostPlugin = new webpack.DefinePlugin({
-  'process.env.API_HOST': JSON.stringify(process.env.API_HOST)
+  'process.env.API_HOST': JSON.stringify(apiHost)
 })    
 
 //turn of watching for a one-time build
