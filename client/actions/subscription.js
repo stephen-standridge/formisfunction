a422209a3971c7330be9451641ea2bfad6e1a616
stephen-standridge <stephen@formisfunction.io>
2017-08-02 14:25:43 -0700

    do not read the subscription database on write

--- client/actions/subscription.js
+++ client/actions/subscription.js
@@ -5,19 +5,11 @@ export function subscribe(email) {
   return function(dispatch) {
     dispatch({ type: SUBSCRIPTION_ACTIONS.REQUESTED })
     const safeEmail = encodeURIComponent(email).replace('.', '%2E');
-    database.ref(`subscriptions/${safeEmail}`).once('value').then(function(snap){
-      if(snap.val()){
-        dispatch({ type: SUBSCRIPTION_ACTIONS.SUCCESS })
+    database.ref(`subscriptions/${safeEmail}`).set(true).then(function(error){
+      if(error) {
+        dispatch({ type: SUBSCRIPTION_ACTIONS.FAILURE, error });
       } else {
-        database.ref(`subscriptions/${safeEmail}`).set(true).then(function(error){
-          if(error) {
-            dispatch({ type: SUBSCRIPTION_ACTIONS.FAILURE, error });
-          } else {
-            dispatch({ type: SUBSCRIPTION_ACTIONS.SUCCESS })
-          }
-        }).catch(function(error){
-          dispatch({ type: SUBSCRIPTION_ACTIONS.FAILURE, error })
-        })
+        dispatch({ type: SUBSCRIPTION_ACTIONS.SUCCESS })
       }
     }).catch(function(error){
       dispatch({ type: SUBSCRIPTION_ACTIONS.FAILURE, error })
