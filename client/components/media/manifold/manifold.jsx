fcfdd598a398c742b233597b1e26494b2b2924af
stephen-standridge <stephen@formisfunction.io>
2018-01-05 23:57:28 -0800

    clean up manifold file

--- client/components/media/manifold/manifold.jsx
+++ client/components/media/manifold/manifold.jsx
@@ -12,81 +12,82 @@ class ManifoldMedia extends React.Component {
     this.startManifold = this._startManifold.bind(this);
     this.stopManifold = this._stopManifold.bind(this);
     this.initializeManifold = this._initializeManifold.bind(this);
-    this.state = {
-      hideProgress: false
-    }
   }
   classNamesFor(part){
     const { classNames } = this.props;
-
     return `manifold-media__${part} ${classNames && classNames[part] || ''}`
   }
+
   getSlug(props=this.props) {
       const { manifold } = props;
       return manifold && manifold.slug || 'ERROR-no-slug';
   }
+
   getVersion(props=this.props) {
     const { manifold } = props;
     if (!manifold) return 'ERROR-no-version';
     const { program_versions, current_version } = manifold;
     return program_versions && program_versions[current_version] || {};
   }
+
   getVersionId(props=this.props) {
     const { manifold } = props;
     if (!manifold) return 'ERROR-no-version-id';
     const { program_versions, current_version } = manifold;
     return program_versions && program_versions[current_version] && program_versions[current_version].version_id;
   }
+
   componentDidMount(){
     this.componentDidUpdate({ manifold: {} })
   }
+
   componentWillUnmount(){
     this.clearManifold(this.props);
   }
+
   componentDidUpdate(prevProps, prevState) {
-      const { manifold, isActive, get_versions, get_configuration, updating } = this.props;
-      if (!manifold) return;
-      const prevManifold = prevProps.manifold || {};
-      const prevActive = prevProps.isActive;
-      const versionId = this.getVersionId();
+    const { manifold, isActive, get_versions, get_configuration, loading } = this.props;
+    if (!manifold) return;
+    const prevManifold = prevProps.manifold || {};
+    const prevActive = prevProps.isActive;
+    const versionId = this.getVersionId();
 
-      const { urlPrefix, version_id, slug } = manifold;
+    const { urlPrefix, version_id, slug } = manifold;
 
-      if(manifold.slug != prevManifold.slug) {
-        return get_versions(manifold);
-      } else if (!updating) {
-        if(versionId !== this.getVersionId(prevProps)){
-          get_configuration(manifold, this.getVersion());
-        } else {
-          if(isActive && !prevActive) { this._startManifold(); }
-          else if(!isActive && prevActive) { this._stopManifold(); }
-          else if(isActive) { this._startManifold(); }
-          else { this._stopManifold(); }
+    if (manifold.slug != prevManifold.slug) return get_versions(manifold);
+    if (loading) return;
+    if (versionId !== this.getVersionId(prevProps)) return get_configuration(manifold, this.getVersion());
 
-        }
-      }
-
+    if(isActive && !prevActive) { this._startManifold(); }
+    else if(!isActive && prevActive) { this._stopManifold(); }
+    else if(isActive) { this._startManifold(); }
+    else { this._stopManifold(); }
   }
+
   _initializeManifold(prevProps) {
     const { manifold, isActive } = this.props;
-    const { options, slug } = manifold;
+    const { slug } = manifold;
     const versionId = this.getVersionId();
+
     let configuration = window[`${slug}_${versionId}`];
-    console.warn(`${slug}_${versionId}`, this.refs[`${slug}_${versionId}`]);
     this.Manifold.load(`${slug}_${versionId}`, configuration, {
       locateFile: this.locateFile.bind(this),
       locateSource: this.locateFile.bind(this),
       // onInitialize: this.clearManifold.bind(this, prevProps)
     }, this.refs[`${slug}_${versionId}`] );
+
     if (isActive) { this._startManifold(); }
     else { this._stopManifold(); }
   }
+
   _startManifold(){
     this.Manifold.start(`${this.getSlug()}_${this.getVersionId()}`)
   }
+
   _stopManifold(){
     this.Manifold.stop(`${this.getSlug()}_${this.getVersionId()}`)
   }
+
   clearManifold(props=this.props){
     const { manifold } = props;
     const { slug } = manifold;
@@ -94,43 +95,16 @@ class ManifoldMedia extends React.Component {
     if (!versionId || !slug) return;
     this.Manifold.unload(`${slug}_${versionId}`);
   }
+
   configurationFile(){
     const { configuration_url } = actions;
     return configuration_url(this.getSlug(), this.getVersion());
   }
+
   locateFile(url){
     const { file_url } = actions;
     return file_url(this.getSlug(), this.getVersion(), url)
   }
-  print(...args){
-    const text = Array.prototype.slice.call(args).join(' ');
-  }
-  printError(...args){
-    const text = Array.prototype.slice.call(args).join(' ');
-    if (0) { // XXX disabled for safety typeof dump == 'function') {
-      dump(text + '\n'); // fast, straight to the real console
-    } else {
-      console.error(text);
-    }
-  }
-
-  onContextLoss(e){
-    alert('WebGL context lost. You will need to reload the page.');
-    e.preventDefault();
-  }
-
-  // renderLoadingMaybe(){
-  //   return this.state.hideProgress ? null
-  //         : (this.props.needsLoad && <div className="manifold_media__loading"></div>)
-  // }
-  // renderCanvases(){
-  //   const { manifold } = this.props;
-  //   if (!manifold) return <div className={this.classNamesFor('not_found')} />
-
-
-  //   const versionId = this.getVersionId();
-  //   return
-  // }
 
   render(){
     const { manifold, isActive } = this.props;
--- client/reducers/manifold.js
+++ client/reducers/manifold.js
@@ -8,18 +8,18 @@ export default function update(state = initialState, action) {
   const { slug, version_id, payload, error } = action;
   switch(action.type) {
     case MANIFOLD_ACTIONS.VERSIONS_REQUESTED:
-      state = state.setIn([slug, 'updating'], true)
+      state = state.setIn([slug, 'loading'], true)
       state = state.setIn([slug, 'error'], false)
     break;
     case MANIFOLD_ACTIONS.VERSIONS_SUCCESS:
-      state = state.setIn([slug, 'updating'], false)
+      state = state.setIn([slug, 'loading'], false)
       state = state.setIn([slug, 'program_versions'], payload.versions)
       state = state.setIn([slug, 'version_count'], payload.versions && payload.versions.length)
       state = state.setIn([slug, 'current_version'], payload.versions && payload.versions.length - 1 || 0)
       state = state.setIn([slug, 'error'], false)
     break;
     case MANIFOLD_ACTIONS.VERSIONS_FAILURE:
-      state = state.setIn([slug, 'updating'], false);
+      state = state.setIn([slug, 'loading'], false);
       state = state.setIn([slug, 'error'], error);
     break;
     case MANIFOLD_ACTIONS.CONFIGURATION_REQUESTED:
